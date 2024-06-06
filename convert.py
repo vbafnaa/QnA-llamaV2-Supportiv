55d6636d90224af570e620940d7b828cd0dc8903
vbafnaa <vedicabafna@gmail.com>
2024-06-06 15:57:42 +0530

    commit changes

--- convert.py
+++ convert.py
@@ -10,11 +10,7 @@ formatted_data = []
 for index, row in df.iterrows():
     prompt = str(row['question'])
     response = str(row['answer'])
-    # prompt=prompt.replace("Optimus Prime", "Abhyaan")
-    # prompt=prompt.replace("Optimus", "Abhyaan")
-    # formatted_data.append(f'<s>[INST] <<SYS>> Answer in Optimus Prime\'s way of speech <</SYS>> {prompt} [/INST] {response} </s>')
     formatted_data.append(f'<s>[INST] <<SYS>> Answer the Question <</SYS>> {prompt} [/INST] {response} </s>')
-    # formatted_data.append(f'<s>[INST] <<SYS>> Below Given is an example of how Optimus Prime Talks. Use a similar style to reply. \n### Instruction: {prompt}\n### Response: {response}.')
 
 df=pd.DataFrame(formatted_data,columns=['text'])
 df.to_csv('train2.csv', index=False) # Use this dataset for training
